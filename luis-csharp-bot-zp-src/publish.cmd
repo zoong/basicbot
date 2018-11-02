458065efca8f085a7141a476532710c703a878b9
zoong <zoong@users.noreply.github.com>
2018-11-02 14:59:17 +1100

    Initial commit

---
+++ luis-csharp-bot-zp-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=luis-csharp-bot-zp-Web-Deploy.pubxml -p:Password=ogb6chRRm1cKlFpTAoifHh1pzztmkXlcgGSdBEJqkKLNcPMMnyxxo95xcvht
+
